9a3b782792ff662866e43a69a72b445d74ba735f
Ivan Nikulin <inikulin@c4a10c3.42berlin.de>
2024-03-16 21:28:46 +0100

    typo

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: inikulin <inikulin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 12:28:14 by inikulin          #+#    #+#             */
-/*   Updated: 2024/03/16 17:57:43 by inikulin         ###   ########.fr       */
+/*   Updated: 2024/03/16 21:28:32 by inikulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	main(int argc, char **argv)
 	b = 0;
 	if (argc == 1)
 		return (0);
-	if(!parse_params(&a, argc, argv))
+	if (!parse_params(&a, argc, argv))
 		return (error(0, 0, 0, 0));
 	if ((CUR_DEBUG & STAGE_RESULT_PRINT) > 0)
 		print(a, b, iprinter);
