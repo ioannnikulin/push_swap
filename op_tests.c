37adf91b604a16de3fdfd61e7df147049c61d596
Ivan Nikulin <inikulin@c4a10c3.42berlin.de>
2024-03-16 21:57:29 +0100

    fixing tests

--- op_tests.c
+++ op_tests.c
@@ -6,7 +6,7 @@
 /*   By: inikulin <inikulin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 17:27:18 by inikulin  yb        #+#    #+#             */
-/*   Updated: 2024/02/10 18:14:08 by inikulin         ###   ########.fr       */
+/*   Updated: 2024/03/16 21:44:32 by inikulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,73 +17,77 @@
 #define SZ 55
 
 typedef struct s_testcase {
-	char	*a[10];
-	char	*b[10];
+	int		a[10];
+	int		asz;
+	int		b[10];
+	int		bsz;
 	t_OP	ops[10];
 	int opsz;
-	char	*expa[10];
-	char	*expb[10];
+	int		expa[10];
+	int		expasz;
+	int		expb[10];
+	int		expbsz;
 }	t_testcase;
 
 void	op_tests()
 {
 	t_dlist *a, *b;
 	t_testcase t[SZ];
-	t[0] = (t_testcase){{"1", "2", "3", NULL}, {"4", "5", "6", "7", NULL}, {PB}, 1, {"2", "3", NULL}, {"1", "4", "5", "6", "7", NULL}};
-	t[1] = (t_testcase){{"1", NULL}, {"4", "5", "6", "7", NULL}, {PB}, 1, {NULL}, {"1", "4", "5", "6", "7", NULL}};
-	t[2] = (t_testcase){{NULL}, {"4", "5", "6", "7", NULL}, {PB}, 1, {NULL}, {"4", "5", "6", "7", NULL}};
-	t[3] = (t_testcase){{"1", NULL}, {"7", NULL}, {PB}, 1, {NULL}, {"1", "7", NULL}};
-	t[4] = (t_testcase){{"1", NULL}, {NULL}, {PB}, 1, {NULL}, {"1", NULL}};
-	t[5] = (t_testcase){{NULL}, {"1", NULL}, {PB}, 1, {NULL}, {"1", NULL}};
-	t[6] = (t_testcase){{NULL}, {NULL}, {PB}, 1, {NULL}, {NULL}};
-	t[7] = (t_testcase){{"4", "5", "6", "7", NULL}, {"1", "2", "3", NULL}, {PA}, 1, {"1", "4", "5", "6", "7", NULL}, {"2", "3", NULL}};
-	t[8] = (t_testcase){{"4", "5", "6", "7", NULL}, {"1", NULL}, {PA}, 1, {"1", "4", "5", "6", "7", NULL}, {NULL}};
-	t[9] = (t_testcase){{"4", "5", "6", "7", NULL}, {NULL}, {PA}, 1, {"4", "5", "6", "7", NULL}, {NULL}};
-	t[10] = (t_testcase){{"7", NULL}, {"1", NULL}, {PA}, 1, {"1", "7", NULL}, {NULL}};
-	t[11] = (t_testcase){{"1", NULL}, {NULL}, {PA}, 1, {"1", NULL}, {NULL}};
-	t[12] = (t_testcase){{NULL}, {"1", NULL}, {PA}, 1, {"1", NULL}, {NULL}};
-	t[13] = (t_testcase){{NULL}, {NULL}, {PA}, 1, {NULL}, {NULL}};
-	t[14] = (t_testcase){{"1", "2", "3", NULL}, {"4", "5", "6", "7", NULL}, {RA}, 1, {"2", "3", "1", NULL}, {"4", "5", "6", "7", NULL}};
-	t[15] = (t_testcase){{"1", NULL}, {"4", "5", "6", "7", NULL}, {RA}, 1, {"1", NULL}, {"4", "5", "6", "7", NULL}};
-	t[16] = (t_testcase){{NULL}, {"4", "5", "6", "7", NULL}, {RA}, 1, {NULL}, {"4", "5", "6", "7", NULL}};
-	t[17] = (t_testcase){{"4", "5", "6", "7", NULL}, {"1", "2", "3", NULL}, {RB}, 1, {"4", "5", "6", "7", NULL}, {"2", "3", "1", NULL}};
-	t[18] = (t_testcase){{"4", "5", "6", "7", NULL}, {"1", NULL}, {RB}, 1, {"4", "5", "6", "7", NULL}, {"1", NULL}};
-	t[19] = (t_testcase){{"4", "5", "6", "7", NULL}, {NULL}, {RB}, 1, {"4", "5", "6", "7", NULL}, {NULL}};
-	t[20] = (t_testcase){{"4", "5", "6", "7", NULL}, {"1", "2", "3", NULL}, {RR}, 1, {"5", "6", "7", "4", NULL}, {"2", "3", "1", NULL}};
-	t[21] = (t_testcase){{"4", "5", "6", "7", NULL}, {"1", NULL}, {RR}, 1, {"5", "6", "7", "4", NULL}, {"1", NULL}};
-	t[22] = (t_testcase){{"4", "5", "6", "7", NULL}, {NULL}, {RR}, 1, {"5", "6", "7", "4", NULL}, {NULL}};
-	t[23] = (t_testcase){{"1", NULL}, {"4", "5", "6", "7", NULL}, {RR}, 1, {"1", NULL}, {"5", "6", "7", "4", NULL}};
-	t[24] = (t_testcase){{NULL}, {"4", "5", "6", "7", NULL}, {RR}, 1, {NULL}, {"5", "6", "7", "4", NULL}};
-	t[25] = (t_testcase){{NULL}, {NULL}, {RR}, 1, {NULL}, {NULL}};
-	t[26] = (t_testcase){{"2", "3", "1", NULL}, {"4", "5", "6", "7", NULL}, {RRA}, 1, {"1", "2", "3", NULL}, {"4", "5", "6", "7", NULL}};
-	t[27] = (t_testcase){{"1", NULL}, {"4", "5", "6", "7", NULL}, {RRA}, 1, {"1", NULL}, {"4", "5", "6", "7", NULL}};
-	t[28] = (t_testcase){{NULL}, {"4", "5", "6", "7", NULL}, {RRA}, 1, {NULL}, {"4", "5", "6", "7", NULL}};
-	t[29] = (t_testcase){{"4", "5", "6", "7", NULL}, {"2", "3", "1", NULL}, {RRB}, 1, {"4", "5", "6", "7", NULL}, {"1", "2", "3", NULL}};
-	t[30] = (t_testcase){{"4", "5", "6", "7", NULL}, {"1", NULL}, {RRB}, 1, {"4", "5", "6", "7", NULL}, {"1", NULL}};
-	t[31] = (t_testcase){{"4", "5", "6", "7", NULL}, {NULL}, {RRB}, 1, {"4", "5", "6", "7", NULL}, {NULL}};
-	t[32] = (t_testcase){{"5", "6", "7", "4", NULL}, {"2", "3", "1", NULL}, {RRR}, 1, {"4", "5", "6", "7", NULL}, {"1", "2", "3", NULL}};
-	t[33] = (t_testcase){{"5", "6", "7", "4", NULL}, {"1", NULL}, {RRR}, 1, {"4", "5", "6", "7", NULL}, {"1", NULL}};
-	t[34] = (t_testcase){{"5", "6", "7", "4", NULL}, {NULL}, {RRR}, 1, {"4", "5", "6", "7", NULL}, {NULL}};
-	t[35] = (t_testcase){{"1", NULL}, {"5", "6", "7", "4", NULL}, {RRR}, 1, {"1", NULL}, {"4", "5", "6", "7", NULL}};
-	t[36] = (t_testcase){{NULL}, {"5", "6", "7", "4", NULL}, {RRR}, 1, {NULL}, {"4", "5", "6", "7", NULL}};
-	t[37] = (t_testcase){{NULL}, {NULL}, {RRR}, 1, {NULL}, {NULL}};
-	t[38] = (t_testcase){{"1", "2", "3", NULL}, {"4", "5", "6", "7", NULL}, {SA}, 1, {"2", "1", "3", NULL}, {"4", "5", "6", "7", NULL}};
-	t[39] = (t_testcase){{"1", NULL}, {"4", "5", "6", "7", NULL}, {SA}, 1, {"1", NULL}, {"4", "5", "6", "7", NULL}};
-	t[40] = (t_testcase){{NULL}, {"4", "5", "6", "7", NULL}, {SA}, 1, {NULL}, {"4", "5", "6", "7", NULL}};
-	t[41] = (t_testcase){{"4", "5", "6", "7", NULL}, {"1", "2", "3", NULL}, {SB}, 1, {"4", "5", "6", "7", NULL}, {"2", "1", "3", NULL}};
-	t[42] = (t_testcase){{"4", "5", "6", "7", NULL}, {"1", NULL}, {SB}, 1, {"4", "5", "6", "7", NULL}, {"1", NULL}};
-	t[43] = (t_testcase){{"4", "5", "6", "7", NULL}, {NULL}, {SB}, 1, {"4", "5", "6", "7", NULL}, {NULL}};
-	t[44] = (t_testcase){{"4", "5", "6", "7", NULL}, {"1", "2", "3", NULL}, {SS}, 1, {"5", "4", "6", "7", NULL}, {"2", "1", "3", NULL}};
-	t[45] = (t_testcase){{"4", "5", "6", "7", NULL}, {"1", NULL}, {SS}, 1, {"5", "4", "6", "7", NULL}, {"1", NULL}};
-	t[46] = (t_testcase){{"4", "5", "6", "7", NULL}, {NULL}, {SS}, 1, {"5", "4", "6", "7", NULL}, {NULL}};
-	t[47] = (t_testcase){{"1", NULL}, {"4", "5", "6", "7", NULL}, {SS}, 1, {"1", NULL}, {"5", "4", "6", "7", NULL}};
-	t[48] = (t_testcase){{NULL}, {"4", "5", "6", "7", NULL}, {SS}, 1, {NULL}, {"5", "4", "6", "7", NULL}};
-	t[49] = (t_testcase){{"1", NULL}, {"4", NULL}, {SS}, 1, {"1", NULL}, {"4", NULL}};
-	t[50] = (t_testcase){{NULL}, {NULL}, {SS}, 1, {NULL}, {NULL}};
-	t[51] = (t_testcase){{"1", "2", NULL}, {NULL}, {PB}, 1, {"2", NULL}, {"1", NULL}};
-	t[52] = (t_testcase){{NULL}, {"1", "2", NULL}, {PA}, 1, {"1", NULL}, {"2", NULL}};
-	t[53] = (t_testcase){{"1", "2", NULL}, {NULL}, {PB, PB}, 2, {NULL}, {"2", "1", NULL}};
-	t[54] = (t_testcase){{NULL}, {"1", "2", NULL}, {PA, PA}, 2, {"2", "1", NULL}, {NULL}};
+	t[0] = (t_testcase){{1, 2, 3}, 3, {4, 5, 6, 7}, 4, {PB}, 1, {2, 3}, 2, {1, 4, 5, 6, 7}, 5};
+	t[1] = (t_testcase){{1}, 1, {4, 5, 6, 7}, 4, {PB}, 1, {}, 0, {1, 4, 5, 6, 7}, 5};
+	t[2] = (t_testcase){{}, 0, {4, 5, 6, 7}, 4, {PB}, 1, {}, 0, {4, 5, 6, 7}, 4};
+	t[3] = (t_testcase){{1}, 1, {7}, 1, {PB}, 1, {}, 0, {1, 7}, 2};
+	t[4] = (t_testcase){{1}, 1, {}, 0, {PB}, 1, {}, 0, {1}, 1};
+	t[5] = (t_testcase){{}, 0, {1}, 1, {PB}, 1, {}, 0, {1}, 1};
+	t[6] = (t_testcase){{}, 0, {}, 0, {PB}, 1, {}, 0, {}, 0};
+	t[7] = (t_testcase){{4, 5, 6, 7}, 4, {1, 2, 3}, 3, {PA}, 1, {1, 4, 5, 6, 7}, 5, {2, 3}, 2};
+	t[8] = (t_testcase){{4, 5, 6, 7}, 4, {1}, 1, {PA}, 1, {1, 4, 5, 6, 7}, 5, {}, 0};
+	t[9] = (t_testcase){{4, 5, 6, 7}, 4, {}, 0, {PA}, 1, {4, 5, 6, 7}, 4, {}, 0};
+	t[10] = (t_testcase){{7}, 1, {1}, 1, {PA}, 1, {1, 7}, 2, {}, 0};
+	t[11] = (t_testcase){{1}, 1, {}, 0, {PA}, 1, {1}, 1, {}, 0};
+	t[12] = (t_testcase){{}, 0, {1}, 1, {PA}, 1, {1}, 1, {}, 0};
+	t[13] = (t_testcase){{}, 0, {}, 0, {PA}, 1, {}, 0, {}, 0};
+	t[14] = (t_testcase){{1, 2, 3}, 3, {4, 5, 6, 7}, 4, {RA}, 1, {2, 3, 1}, 3, {4, 5, 6, 7}, 4};
+	t[15] = (t_testcase){{1}, 1, {4, 5, 6, 7}, 4, {RA}, 1, {1}, 1, {4, 5, 6, 7}, 4};
+	t[16] = (t_testcase){{}, 0, {4, 5, 6, 7}, 4, {RA}, 1, {}, 0, {4, 5, 6, 7}, 4};
+	t[17] = (t_testcase){{4, 5, 6, 7}, 4, {1, 2, 3}, 3, {RB}, 1, {4, 5, 6, 7}, 4, {2, 3, 1}, 3};
+	t[18] = (t_testcase){{4, 5, 6, 7}, 4, {1}, 1, {RB}, 1, {4, 5, 6, 7}, 4, {1}, 1};
+	t[19] = (t_testcase){{4, 5, 6, 7}, 4, {}, 0, {RB}, 1, {4, 5, 6, 7}, 4, {}, 0};
+	t[20] = (t_testcase){{4, 5, 6, 7}, 4, {1, 2, 3}, 3, {RR}, 1, {5, 6, 7, 4}, 4, {2, 3, 1}, 3};
+	t[21] = (t_testcase){{4, 5, 6, 7}, 4, {1}, 1, {RR}, 1, {5, 6, 7, 4}, 4, {1}, 1};
+	t[22] = (t_testcase){{4, 5, 6, 7}, 4, {}, 0, {RR}, 1, {5, 6, 7, 4}, 4, {}, 0};
+	t[23] = (t_testcase){{1}, 1, {4, 5, 6, 7}, 4, {RR}, 1, {1}, 1, {5, 6, 7, 4}, 4};
+	t[24] = (t_testcase){{}, 0, {4, 5, 6, 7}, 4, {RR}, 1, {}, 0, {5, 6, 7, 4}, 4};
+	t[25] = (t_testcase){{}, 0, {}, 0, {RR}, 1, {}, 0, {}, 0};
+	t[26] = (t_testcase){{2, 3, 1}, 3, {4, 5, 6, 7}, 4, {RRA}, 1, {1, 2, 3}, 3, {4, 5, 6, 7}, 4};
+	t[27] = (t_testcase){{1}, 1, {4, 5, 6, 7}, 4, {RRA}, 1, {1}, 1, {4, 5, 6, 7}, 4};
+	t[28] = (t_testcase){{}, 0, {4, 5, 6, 7}, 4, {RRA}, 1, {}, 0, {4, 5, 6, 7}, 4};
+	t[29] = (t_testcase){{4, 5, 6, 7}, 4, {2, 3, 1}, 3, {RRB}, 1, {4, 5, 6, 7}, 4, {1, 2, 3}, 3};
+	t[30] = (t_testcase){{4, 5, 6, 7}, 4, {1}, 1, {RRB}, 1, {4, 5, 6, 7}, 4, {1}, 1};
+	t[31] = (t_testcase){{4, 5, 6, 7}, 4, {}, 0, {RRB}, 1, {4, 5, 6, 7}, 4, {}, 0};
+	t[32] = (t_testcase){{5, 6, 7, 4}, 4, {2, 3, 1}, 3, {RRR}, 1, {4, 5, 6, 7}, 4, {1, 2, 3}, 3};
+	t[33] = (t_testcase){{5, 6, 7, 4}, 4, {1}, 1, {RRR}, 1, {4, 5, 6, 7}, 4, {1}, 1};
+	t[34] = (t_testcase){{5, 6, 7, 4}, 4, {}, 0, {RRR}, 1, {4, 5, 6, 7}, 4, {}, 0};
+	t[35] = (t_testcase){{1}, 1, {5, 6, 7, 4}, 4, {RRR}, 1, {1}, 1, {4, 5, 6, 7}, 4};
+	t[36] = (t_testcase){{}, 0, {5, 6, 7, 4}, 4, {RRR}, 1, {}, 0, {4, 5, 6, 7}, 4};
+	t[37] = (t_testcase){{}, 0, {}, 0, {RRR}, 1, {}, 0, {}, 0};
+	t[38] = (t_testcase){{1, 2, 3}, 3, {4, 5, 6, 7}, 4, {SA}, 1, {2, 1, 3}, 3, {4, 5, 6, 7}, 4};
+	t[39] = (t_testcase){{1}, 1, {4, 5, 6, 7}, 4, {SA}, 1, {1}, 1, {4, 5, 6, 7}, 4};
+	t[40] = (t_testcase){{}, 0, {4, 5, 6, 7}, 4, {SA}, 1, {}, 0, {4, 5, 6, 7}, 4};
+	t[41] = (t_testcase){{4, 5, 6, 7}, 4, {1, 2, 3}, 3, {SB}, 1, {4, 5, 6, 7}, 4, {2, 1, 3}, 3};
+	t[42] = (t_testcase){{4, 5, 6, 7}, 4, {1}, 1, {SB}, 1, {4, 5, 6, 7}, 4, {1}, 1};
+	t[43] = (t_testcase){{4, 5, 6, 7}, 4, {}, 0, {SB}, 1, {4, 5, 6, 7}, 4, {}, 0};
+	t[44] = (t_testcase){{4, 5, 6, 7}, 4, {1, 2, 3}, 3, {SS}, 1, {5, 4, 6, 7}, 4, {2, 1, 3}, 3};
+	t[45] = (t_testcase){{4, 5, 6, 7}, 4, {1}, 1, {SS}, 1, {5, 4, 6, 7}, 4, {1}, 1};
+	t[46] = (t_testcase){{4, 5, 6, 7}, 4, {}, 0, {SS}, 1, {5, 4, 6, 7}, 4, {}, 0};
+	t[47] = (t_testcase){{1}, 1, {4, 5, 6, 7}, 4, {SS}, 1, {1}, 1, {5, 4, 6, 7}, 4};
+	t[48] = (t_testcase){{}, 0, {4, 5, 6, 7}, 4, {SS}, 1, {}, 0, {5, 4, 6, 7}, 4};
+	t[49] = (t_testcase){{1}, 1, {4}, 1, {SS}, 1, {1}, 1, {4}, 1};
+	t[50] = (t_testcase){{}, 0, {}, 0, {SS}, 1, {}, 0, {}, 0};
+	t[51] = (t_testcase){{1, 2}, 2, {}, 0, {PB}, 1, {2}, 1, {1}, 1};
+	t[52] = (t_testcase){{}, 0, {1, 2}, 2, {PA}, 1, {1}, 1, {2}, 1};
+	t[53] = (t_testcase){{1, 2}, 2, {}, 0, {PB, PB}, 2, {}, 0, {2, 1}, 2};
+	t[54] = (t_testcase){{}, 0, {1, 2}, 2, {PA, PA}, 2, {2, 1}, 2, {}, 0};
 	printf("Starting %d dlist-operational tests.\n", SZ - START);
 	for (int i = START; i < SZ; i ++)
 	{
@@ -91,12 +95,12 @@ void	op_tests()
 			printf("\n################################################################\n\n");
 		if ((CUR_DEBUG & OP_TEST_NUMBERS) > 0)
 			printf("test #%i\n", i);
-		a = ft_dlist_generate(t[i].a, 0);
-		b = ft_dlist_generate(t[i].b, 0);
+		a = ft_dlist_generate_int(t[i].a, t[i].asz, 0);
+		b = ft_dlist_generate_int(t[i].b, t[i].bsz, 0);
 		if ((CUR_DEBUG & OP_TEST_FULLDETAILS) > 0)
 		{
 			printf("ORIGINAL:\n");
-			assert(print(a, b, sprinter) == 0);
+			assert(print(a, b, iprinter) == 0);
 		}
 		int op = t[i].opsz;
 		while (op --)
@@ -143,26 +147,26 @@ void	op_tests()
 		}
 		int nsza = ft_dlist_size(a);
 		int nszb = ft_dlist_size(b);
-		t_dlist *expa = ft_dlist_generate(t[i].expa, 0);
-		t_dlist *expb = ft_dlist_generate(t[i].expb, 0);
+		t_dlist *expa = ft_dlist_generate_int(t[i].expa, t[i].expasz, 0);
+		t_dlist *expb = ft_dlist_generate_int(t[i].expb, t[i].expbsz, 0);
 		int expsza = ft_dlist_size(expa);
 		int expszb = ft_dlist_size(expb);
 		if ((CUR_DEBUG & OP_TEST_DETAILS) > 0)
 			printf("a: %i nodes (expected %i), b: %i nodes (expected %i)\n", nsza, expsza, nszb, expszb);
-		int wronga = ft_dlist_ncmp_str(a, expa, expsza + 1) != 0;
-		int wrongb = ft_dlist_ncmp_str(b, expb, expszb + 1) != 0;
+		int wronga = ft_dlist_ncmp_i(a, expa, expsza + 1) != 0;
+		int wrongb = ft_dlist_ncmp_i(b, expb, expszb + 1) != 0;
 		if ((CUR_DEBUG & OP_TEST_FULLDETAILS) > 0 && (wronga || wrongb || (CUR_DEBUG & OP_TEST_FULLDETAILS_SUCCESS) > 0))
 		{
 			printf("EXPECTED:\n");
-			assert(print(expa, expb, sprinter) == 0);
+			assert(print(expa, expb, iprinter) == 0);
 			printf("ACTUAL:\n");
-			assert(print(a, b, sprinter) == 0);
+			assert(print(a, b, iprinter) == 0);
 		}
 		assert(!wronga && !wrongb);
-		ft_dlist_clear(&a, free, 0);
-		ft_dlist_clear(&b, free, 0);
-		ft_dlist_clear(&expa, free, 0);
-		ft_dlist_clear(&expb, free, 0);
+		ft_dlist_clear(&a, 0, 0);
+		ft_dlist_clear(&b, 0, 0);
+		ft_dlist_clear(&expa, 0, 0);
+		ft_dlist_clear(&expb, 0, 0);
 	}
 	printf("SUCCESS: all operations performed as predicted\n");
 }
